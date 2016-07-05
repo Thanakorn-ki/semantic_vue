877220afcbf80ed5117c893ada26c498d4b84f11
salapao2136 <thanakornaum11@gmail.com>
2016-07-05 15:57:28 +0700

    new

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template lang="html">
+  <div class="ui labeled button" tabindex="0"  @click="alertmodal()">
+  <div class="ui red button">
+    <i class="heart icon"></i> Like
+  </div>
+  <a class="ui basic red left pointing label">
+    1,048
+  </a>
+</div>
+<div class="ui labeled button" tabindex="0">
+  <div class="ui basic blue button">
+    <i class="fork icon"></i> Forks
+  </div>
+  <a class="ui basic left pointing blue label">
+    1,048
+  </a>
+</div>
+
+<div class="ui basic modal">
+  <i class="close icon"></i>
+  <div class="header">
+    Archive Old Messages
+  </div>
+  <div class="image content">
+    <div class="image">
+      <i class="archive icon"></i>
+    </div>
+    <div class="description">
+      <p>Your inbox is getting full, would you like us to enable automatic archiving of old messages?</p>
+    </div>
+  </div>
+  <div class="actions">
+    <div class="two fluid ui inverted buttons">
+      <div class="ui cancel red basic inverted button">
+        <i class="remove icon"></i>
+        No
+      </div>
+      <div class="ui ok green basic inverted button">
+        <i class="checkmark icon"></i>
+        Yes
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+/*globals $:false */
+
+export default {
+  data: function () {
+    return {
+    }
+  },
+  computed: {},
+  ready: function () {},
+  attached: function () {},
+  methods: {
+    alertmodal: function () {
+      $('.ui.basic.modal').modal('show')
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+import '../node_modules/semantic-ui/dist/semantic.min.css'
+import semantic from 'semantic'
+console.log(semantic)
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App }
+})
