b976047e553e7ae1ca8d4eca1ced382e6c3141a5
salapao2136 <thanakornaum11@gmail.com>
2016-07-06 14:41:53 +0700

    last

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import '../node_modules/semantic-ui/dist/semantic.min.css'
-import semantic from 'semantic'
-console.log(semantic)
+import 'semantic'
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
